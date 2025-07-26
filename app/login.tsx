c7345c1389bc24709cd412e941c143a4ede8d17c
taewooyo <skaxodn97@gmail.com>
2025-07-26 21:11:18 +0900

    mirage를 통한 로그인 처리 로직 개발

--- app/login.tsx
+++ app/login.tsx
@@ -1,14 +1,63 @@
-import { Redirect } from "expo-router";
-import { View, Text} from "react-native";
+import { Redirect, router } from "expo-router";
+import { View, Text, Pressable, StyleSheet, Alert } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 export default function Login() {
+    const insets = useSafeAreaInsets();
     const isLoggedIn = false;
+
+    const onLogin = () => {
+        console.log("login");
+
+        fetch("/login", {
+            method: "POST",
+            body: JSON.stringify({
+                username: "stitch",
+                password: "1234",
+            }),
+        })
+            .then((res) => {
+                console.log("res", res, res.status);
+                if(res.status >= 400) {
+                    return Alert.alert("Error", "Invalid credentials");
+                }
+                return res.json()
+            })
+            .then((data) => {
+                console.log("data", data);
+            })
+            .catch((error) => {
+                console.error(error)
+            })
+    }
     if (isLoggedIn) {
-        return <Redirect href="/(tabs)"/>;
+        return <Redirect href="/(tabs)" />;
     }
     return (
-        <View>
-            <Text>Login</Text>
+        <View style={{ paddingTop: insets.top }}>
+            <Pressable onPress={() => router.back()}>
+                <Text>Login</Text>
+            </Pressable>
+            <Pressable
+                style={styles.loginButton}
+                onPress={onLogin}>
+                <Text style={styles.loginButtonText}>
+                    Login
+                </Text>
+            </Pressable>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    loginButton: {
+        backgroundColor: "blue",
+        padding: 10,
+        borderRadius: 5,
+        width: 100,
+        alignItems: "center"
+    },
+    loginButtonText: {
+        color: "white"
+    },
+});
